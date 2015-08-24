1b289ce4be03aac92445a01c8a59ba2de4a8a0d9
Gleidson <gleidson.gmoura@gmail.com>
2015-08-24 00:57:44 -0300

    estudo de classe

---
+++ estudo/src/br/com/igtec/estudo/modelo/Aviao.java
@@ -0,0 +1,12 @@
+package br.com.igtec.estudo.modelo;
+
+public class Aviao {
+	public Pessoa piloto;
+	public Pessoa aeromoca;
+	public Pessoa passageiro;
+	
+    public String destino;
+    
+	
+
+}
---
+++ estudo/src/br/com/igtec/estudo/visao/TesteAviao.java
@@ -0,0 +1,30 @@
+package br.com.igtec.estudo.visao;
+
+import br.com.igtec.estudo.modelo.Aviao;
+import br.com.igtec.estudo.modelo.Pessoa;
+
+public class TesteAviao {
+	public static void main(String[] args) {
+		Aviao aviao = new Aviao();
+		
+		aviao.piloto = new Pessoa();
+		aviao.piloto.nome = "igor";
+		
+		aviao.aeromoca = new Pessoa();
+		aviao.aeromoca.nome = "amanda";
+		
+		aviao.passageiro = new Pessoa();
+		aviao.passageiro.nome = "gleidson";
+		
+		aviao.destino = "paris";
+		
+		
+		System.out.println("Nome do piloto: " + aviao.piloto.nome);
+		System.out.println("Nome da aeromoca: " + aviao.aeromoca.nome);
+		System.out.println("Nome do passageiro: " + aviao.passageiro.nome);
+		System.out.println("Destino a: " + aviao.destino);
+		
+		
+	}
+
+}
